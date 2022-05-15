36c15a344c149518671424de69c7bf6bca729735
LarryZhang <u3594786@connect.hku.hk>
2022-05-15 23:33:48 +0800

    Update code

---
+++ Portal/portal_selenium.py
@@ -0,0 +1,168 @@
+from time import sleep
+from datetime import datetime
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+PORTAL_URL = "https://hkuportal.hku.hk/login.html"
+
+OPENING = "https://sis-main.hku.hk/cs/sisprod/cache/PS_CS_STATUS_OPEN_ICN_1.gif"
+CLOSED = "https://sis-main.hku.hk/cs/sisprod/cache/PS_CS_STATUS_CLOSED_ICN_1.gif"
+SUCCEED = "https://sis-main.hku.hk/cs/sisprod/cache/PS_CS_STATUS_SUCCESS_ICN_1.gif"
+FAILED = "https://sis-main.hku.hk/cs/sisprod/cache/PS_CS_STATUS_ERROR_ICN_1.gif"
+
+with open("pwd.txt", "r") as f:
+    USERNAME, PASSWORD = f.read().split('\n')[:2]
+
+REFRESH_RATE = 5 * 60
+TIMEOUT = 30
+MAX_REATTEMPTS = 3
+MAX_REFRESHES = 2
+
+
+class Enrollee:
+    def __init__(self):
+        self.options = Options()
+        self.options.headless = True
+        self.driver = None
+
+    def start(self):
+        try:
+            self.driver.quit()
+        except:
+            pass
+
+        # self.driver = webdriver.Firefox(options=options)
+        self.driver = webdriver.Chrome()
+        self.driver.get(PORTAL_URL)
+
+        username_input = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#username")))
+        password_input = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#password")))
+        submit = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "input[type=image]")))
+
+        username_input.send_keys(USERNAME)
+        password_input.send_keys(PASSWORD)
+        submit.click()
+
+        add_class_link = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#crefli_Z_HC_SSR_SSENRL_CART_LNK > a")))
+        add_class_link.click()
+
+    def proceed(self):
+        proceed = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located(
+            (By.XPATH, f'//*[@id="DERIVED_REGFRM1_LINK_ADD_ENRL$82$"]')))
+        proceed.click()
+        finish = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#DERIVED_REGFRM1_SSR_PB_SUBMIT")))
+        finish.click()
+
+        results = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_all_elements_located(
+            (By.XPATH, '//*[@id="SSR_SS_ERD_ER$scroll$0"]/tbody/tr/td/table/tbody/tr')))
+        results = results[1:]
+
+        print(datetime.now())
+        all_failed = True
+        for result in results:
+            result_name = WebDriverWait(result, TIMEOUT).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "[id^=win0divR_CLASS_NAME] > span"))).text
+            result_message = WebDriverWait(result, TIMEOUT).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "[id^=win0divDERIVED_REGFRM1_SS_MESSAGE_LONG] > div"))).text
+            result_status = WebDriverWait(result, TIMEOUT).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "[id^=win0divDERIVED_REGFRM1_SSR_STATUS_LONG] img"))).get_attribute(
+                "src")
+
+            if result_status == SUCCEED:
+                print(f"{result_name} Submitted for approval")
+                all_failed = False
+            elif result_status == FAILED:
+                print(f"Unable to add {result_name}:\n{result_message}")
+
+        if all_failed:
+            return 1
+
+        return 0
+
+    def check_status(self):
+        self.driver.refresh()
+
+        frame = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#ptifrmtgtframe")))
+        self.driver.switch_to.frame(frame)
+
+        sem_2 = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "input[id^=SSR_DUMMY_RECV1]")))
+        cont = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, "#win0divDERIVED_SSS_SCT_SSR_PB_GO input")))
+        sem_2.click()
+        cont.click()
+
+        temporary_list = WebDriverWait(self.driver, TIMEOUT).until(EC.presence_of_all_elements_located(
+            (By.XPATH, '//*[@id="SSR_REGFORM_VW$scroll$0"]/tbody/tr[2]/td/table/tbody/tr')))
+        temporary_list = temporary_list[1:]
+
+        print("Refreshed at", datetime.now())
+
+        if temporary_list[0].find_element(By.CSS_SELECTOR, "div").get_attribute("id") == "win0divP_NO_CLASSES$0":
+            print("No class in temporary list")
+            exit(0)
+
+        for course in temporary_list:
+            course_name = WebDriverWait(course, TIMEOUT).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "[id^=P_CLASS_NAME]"))).text.split('\n')[0]
+            course_status = WebDriverWait(course, TIMEOUT).until(EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "[id^=win0divDERIVED_REGFRM1_SSR_STATUS_LONG] img"))).get_attribute("src")
+
+            if course_status == OPENING:
+                print(course_name, "opening, selection proceeding\7")
+                enrol_result = self.proceed()
+                return enrol_result
+
+            elif course_status == CLOSED:
+                print(course_name, "is closed")
+
+        print(f"Will refresh in {REFRESH_RATE} seconds\n")
+        sleep(REFRESH_RATE)
+        return 0
+
+
+def main():
+    reattempts = 0
+    while True:
+        try:
+            enrollee = Enrollee()
+            enrollee.start()
+
+            refreshes = 0
+            while refreshes < MAX_REFRESHES:
+                check_result = enrollee.check_status()
+                if check_result == 1:
+                    refreshes += 1
+                    print(f"Retrying {refreshes}/{MAX_REFRESHES}")
+                    continue
+
+                reattempts = 0
+                refreshes = 0
+
+            raise Exception("Reached maximum refreshes")
+
+        except Exception as e:
+            print(e)
+            reattempts += 1
+
+            if reattempts > MAX_REATTEMPTS:
+                print(f"Reached maximum reattempts, will retry in {REFRESH_RATE} seconds\7\n")
+                sleep(REFRESH_RATE)
+
+            print(f"Restarting webdriver {reattempts}/{MAX_REATTEMPTS}")
+            enrollee.start()
+            continue
+
+
+if __name__ == "__main__":
+    main()
